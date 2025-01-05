8ef46f156f8eefab05861e8dc327c5cbe503e3dd
Thai-dot <nguyenhoangthai7871@gmail.com>
2025-01-05 15:11:29 +0700

    add pagination post service

--- web-app/src/configurations/configuration.js
+++ web-app/src/configurations/configuration.js
@@ -4,5 +4,6 @@ export const CONFIG = {
 
 export const API = {
   LOGIN: "/identity/auth/token",
-  MY_INFO: "/identity/users/my-info",
+  MY_INFO: "/profile/user-profile/my-profile",
+  MY_POST: "/post/my-posts",
 };
