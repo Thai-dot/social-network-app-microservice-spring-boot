38413817d53aa55bff9ef392e510f2b06790b153
Thai-dot <nguyenhoangthai7871@gmail.com>
2025-01-23 11:51:09 +0700

    add search

--- profile-service/src/main/java/com/thaidot/event/dto/ProfileEvent.java
+++ profile-service/src/main/java/com/thaidot/event/dto/ProfileEvent.java
@@ -1,4 +1,21 @@
 package com.thaidot.event.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class ProfileEvent {
+    private String userID;
+    private String firstName;
+    private String lastName;
+    private String city;
+    private LocalDate dob;
+    private String avatarUrl;
 }
--- profile-service/src/main/java/com/thaidot/profile/service/KafkaProducer.java
+++ profile-service/src/main/java/com/thaidot/profile/service/KafkaProducer.java
@@ -1,4 +1,22 @@
 package com.thaidot.profile.service;
 
+import com.thaidot.event.dto.ProfileEvent;
+import com.thaidot.event.dto.UpdateAvatarProfileEvent;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class KafkaProducer {
+
+    private final KafkaTemplate<String, Object> kafkaTemplate;
+
+    @Value("${kafka-topic.add-profile-elastic}")
+    private String addProfileElasticTopic;
+
+    public void sendAddProfileElastic(ProfileEvent event) {
+        kafkaTemplate.send(addProfileElasticTopic, event);
+    }
 }
